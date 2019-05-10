cc97daf5fc55b30ec52ae1a27005184599096ee0
Amin <m.amin.kashi@gmail.com>
2019-05-10 22:32:06 +0430

    Update tracetrack.py

--- racetrack.py
+++ racetrack.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 from IPython.display import clear_output
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 OBSTACLE = 1
 GOAL = 2
@@ -19,39 +19,39 @@ class Agent():
     """Agent to find best direction to goal.
 
     Arguments:
-        map_shape: Shape of map
-        goal: Location of goal
-        theta: A small threshold determining accuracy of estimation
-        obstacle_list: List of obstacles
-        alpha: Probability of random obstacle
-        slip_probability: The probability of slip in the direction of movement
+        grid_map: Shape of map
+        constant_speed_probability: The probability at each time step that the velocity increments are both zero
                           (making the environment nondeterministic)
     """
 
     def __init__(self,
                  grid_map,
-                 theta=0.001,
                  constant_speed_probability=0):
         self.grid_map = grid_map
-        self.theta = theta
         self.constant_speed_probability = constant_speed_probability
         self.A = [(i, j) for i in range(-1, 2) for j in range(-1, 2)]
-        self.S = [(x, y, vx, vy) for x in range(0, self.grid_map.shape[0]) for y in range(0, self.grid_map.shape[1])
-                  for vx in range(-5, 6) for vy in range(-5, 6)]
+
+    def debug(self, data):
+        print(data)
+        # sleep(0.5)
+        clear_output(wait=True)
 
     def random_start(self):
+        """ Returning a random point from all start points"""
         start_points = np.where(self.grid_map == START)
         index = np.int16(np.random.uniform(0, start_points[0].shape[0]))
         return (start_points[0][index], start_points[1][index])
 
     def deterministic_move(self, s, a):
+        """Returns next state and immediate reward
+        follows by state s and action a"""
 
         # next velocity
-        v_n = tuple(np.array(self.S[s][2:]) + np.array(self.A[a]))
+        v_n = tuple(np.array(s[2:]) + np.array(self.A[a]))
         # velocity limmit between 0 and 5
         v_n = (min(5, max(-5, v_n[0])), min(5, max(-5, v_n[1])))
         # next location
-        l_n = tuple(np.array(self.S[s][:2]) + np.array(v_n))
+        l_n = tuple(np.array(s[:2]) + np.array(v_n))
         # location limmit in grid_map
         l_n = (min(self.grid_map.shape[0]-1, max(0, l_n[0])),
                min(self.grid_map.shape[1]-1, max(0, l_n[1])))
@@ -63,10 +63,11 @@ class Agent():
             v_n = (0, 0)
         if self.grid_map[l_n] == GOAL:
             r = P_REWARD
-        s_n = self.S.index(l_n + v_n)
+        s_n = l_n + v_n
         return s_n, r
 
     def environment(self, s, a):
+        
         if np.random.uniform() < self.constant_speed_probability:
             return self.deterministic_move(s, ZERO_ACCELERATION)
         return self.deterministic_move(s, a)
@@ -74,75 +75,48 @@ class Agent():
     def episode_generator(self):
 
         # first state (random)
-        s = self.S.index(self.random_start() + (0, 0))
+        s = self.random_start() + (0, 0)
         # a log list for episode
         episode = list()
 
         while True:
             a = np.int16(np.random.uniform(0, 9))
             s, r = self.environment(s, a)
-            episode.append((s, a, r, 1.0/9))
-            if self.grid_map[self.S[s][:2]] == GOAL:
+            episode.append(s + (a, r, 1.0/9))
+            if self.grid_map[s[:2]] == GOAL:
                 return episode
 
-    # def value_iteration(self):
+    def value_iteration(self, iteration):
 
-    #     # 1. Initialization
-    #     S = [(x, y, vx, vy) for x in range(0, self.grid_map.shape[0]) for y in range(0, self.grid_map.shape[1])
-    #          for vx in range(0, 11) for vy in range(0, 11)]
+        # 1. Initialization
+        Q = np.ones(
+            shape=(self.grid_map.shape[0], self.grid_map.shape[1], 11, 11, 9)) * ZERO_ACCELERATION
+        C = np.zeros(
+            shape=(self.grid_map.shape[0], self.grid_map.shape[1], 11, 11, 9))
+        pi = np.ones(
+            shape=(self.grid_map.shape[0], self.grid_map.shape[1], 11, 11)) * ZERO_ACCELERATION
 
-    #     # V = np.random.rand(self.grid_map.size[0], self.grid_map.size[1], 6, 6)
-    #     Q = np.zeros(shape=(self.grid_map[0], self.grid_map[1], 11, 11, 9))
-    #     C = np.zeros(shape=(self.grid_map[0], self.grid_map[1], 11, 11, 9))
-    #     pi = [np.int16(np.random.uniform(0, 10)) for state in S]
-
-    #     x = 0
-    #     # 2. Policy Evaluation  & Policy Improvement
-    #     while True:
-    #         delta = 0.0
-    #         x = x + 1
-    #         episode = self.episode_generator
-    #         G = 0
-    #         W = 1
-    #         for e in reversed(episode):
-    #             r = e[6]
-    #             s = e[:4]
-    #             G = r + gamma*G
-    #             # C[] = C + W
-
-    #             if self.map[s] == GOAL:
-    #                 V[s] = 0
-    #             elif self.map[s] == OBSTACLE:
-    #                 V[s] = -1
-    #             else:
-    #                 maximum_pi = 0
-    #                 for a in self.A:
-    #                     summation = 0
-    #                     state_reward = self.environment(s, a)
-    #                     for s_r in state_reward:
-    #                         summation = summation + s_r[2] * (
-    #                             s_r[1] + 0.9 * V[s_r[0]])
-    #                     Q[s[0], s[1], self.A.index(a)] = summation
-    #                     if summation > maximum_pi:
-    #                         maximum_pi = summation
-    #                         V[s] = summation
-    #                         pi[s] = self.A.index(a)
-
-    #             delta = max(delta, abs(v - V[s]))
-
-    #         pi_set = np.append(pi_set, [pi], 0)
-    #         V_set = np.append(V_set, [V], 0)
-    #         if delta < self.theta:
-    #             break
-
-    #         if x > 5000:
-    #             print("Probably there are some closed area in the map!")
-    #             break
-    #     return V_set, pi_set, Q
-
-
-
-
+        x = 0
+        # 2. Policy Evaluation  & Policy Improvement
+        while x < iteration:
+            self.debug(x/np.float(iteration))
+            gamma = 0.9
+            x = x + 1
+            episode = self.episode_generator()
+            G = 0
+            W = 1
+            for e in reversed(episode):
+                r = e[5]
+                a = e[4]
+                p = e[6]
+                G = r + gamma*G
+                C[e[:-2]] = C[e[:-2]] + W
+                Q[e[:-2]] = Q[e[:-2]] + W/C[e[:-2]]*(G - Q[e[:-2]])
+                pi[e[:-3]] = np.argmax(Q[e[:-3]])
+                if pi[e[:-3]] != a:
+                    break
+                W = W * 1 / p
+        return pi
 
     # def arrow(self, i, j, pi):
     #     """ Map for policy"""
